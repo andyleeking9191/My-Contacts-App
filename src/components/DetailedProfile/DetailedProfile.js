459137bcd58c01490c93befb4d9aa7743719b2fa
Andrew <andyleeking9191@yahoo.com>
2022-01-09 20:34:06 -0500

    Added initail elements and styles to the detailed profile component

--- src/components/DetailedProfile/DetailedProfile.js
+++ src/components/DetailedProfile/DetailedProfile.js
@@ -2,15 +2,37 @@ import React from "react";
 import "./DetailedProfile.css";
 
 const DetailedProfile = (props) => {
-
-  console.log(props.profileData)
+  const profile = props.profileData;
+  console.log(profile);
 
   return (
     <div className="detailed-profile">
-      <p>{props.profileData.firstName}</p>
-      <button className="profile__btn" onClick={props.closeDetailedProfileHandler}>
-        Close detailed view
+      <button
+        className="profile__btn--close"
+        onClick={props.closeDetailedProfileHandler}
+      >
+        X
       </button>
+      <div className="detailed-profile__top-info-container">
+        <img
+          alt="profile"
+          className="detailed-profile__image"
+          src={profile.image}
+        ></img>
+        <div className="detailed-profile__top-info-text">
+        <p>
+          {profile.firstName} {profile.lastName}
+        </p>
+        <p>{profile.cell}</p>
+        <p>{profile.email}</p>
+        </div>
+      </div>
+      <p>
+        {profile.addressNum} {profile.street}
+      </p>
+      <p>
+        {profile.city}, {profile.state} {profile.postcode}
+      </p>
     </div>
   );
 };
